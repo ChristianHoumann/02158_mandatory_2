5c860aedaa1b26c9d4efb14a85e1893ab281bb67
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2022-10-06 11:00:04 +0200

    Add solution for problem 2

--- src/Field.java
+++ src/Field.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 //Prototype implementation of Field class
 //Mandatory assignment 2
 //Course 02158 Concurrent Programming, DTU, Fall 2022
@@ -6,18 +8,26 @@
 
 public class Field {
 
-    public Field() {
+    // Matrix (nested arraylist) of semaphores for each field
+    private ArrayList<ArrayList<Semaphore>> fieldMutex = new ArrayList<ArrayList<Semaphore>>();
 
+    public Field() {
+        for (int i = 0; i < 11; i++) {
+            this.fieldMutex.add(new ArrayList<Semaphore>());
+            for (int j = 0; j < 12; j++) {
+                this.fieldMutex.get(i).add(new Semaphore(1));
+            }
+        }
     }
 
     /* Block until car no. may safely enter tile at pos */
     public void enter(int no, Pos pos) throws InterruptedException {
-
+        this.fieldMutex.get(pos.row).get(pos.col).P();
     }
 
     /* Release tile at position pos */
     public void leave(Pos pos) {
-
+        this.fieldMutex.get(pos.row).get(pos.col).V();
     }
 
 }
